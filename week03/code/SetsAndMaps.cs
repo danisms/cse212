9823eac1368ae84e755d9eb1cba8cc3824203fea
Daniel C. Opute <danielcopute@gmail.com>
2025-07-21 13:49:43 +0100

    completed week 03 coding activities

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 public static class SetsAndMaps
 {
@@ -17,13 +19,41 @@ public static class SetsAndMaps
     /// As a special case, if the letters are the same (example: 'aa') then
     /// it would not match anything else (remember the assumption above
     /// that there were no duplicates) and therefore should not be returned.
+    ///
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
+        // SOLUTION:
+        // step 1: create a set out of the inputed words
+        // step 2: create another empty set to store paires of words found
+        // step 3: loop through the created words set
+        // step 4: invert word, make sure the inverted word is not same as initia word and search for the inverted word in the set
+        // step 5: if inverted word is found remove the word from the set
+        // step 6: add words found into the created empty set
+        // step 7: at the end of the loop, return the found paried words set.
         // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var wordsSet = new HashSet<string>(words);
+        var pairedWords = new HashSet<string>();
+        foreach (string word in wordsSet)
+        {
+            // invert word
+            string invertedWord = $"{word[1]}{word[0]}";
+            if (word != invertedWord)
+            {
+                if (wordsSet.Contains(invertedWord))
+                {
+                    // add word and inverted word into pairedWords set
+                    pairedWords.Add($"{word} & {invertedWord}");
+                    // remove inverted word from wordsSet
+                    wordsSet.Remove(invertedWord);
+                }
+            }
+        }
+
+        return [.. pairedWords];  // convert to an array of strings.
     }
+
 
     /// <summary>
     /// Read a census file and summarize the degrees (education)
@@ -38,11 +68,25 @@ public static class SetsAndMaps
     /// <returns>fixed array of divisors</returns>
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
+        // SOLUTION:
+        // get degree from fields;
+        // check if degree isn't present in degrees dictionary;
+        // if not present add degree to degrees and set the value to 1;
+        // if present increment the value of the degree by 1;
         var degrees = new Dictionary<string, int>();
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
             // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];  // get degree
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree] += 1;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
@@ -66,8 +110,99 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
+        // SOLUTION:
+        // step 1: check if word one is same as word two and return false if true; else;
+        // step 2: convert words into lowercase and remove spaces;
+        // step 3: create a dictionary of both words seperately, using the letters as the key and 
+        // the value representing the number of times the letters appears;
+        // step 4: check if both dictionaries have the same length
+        // finally compare both words keys and values if same return true, else return false;
         // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+
+        // check if both words are same
+        if (word1 == word2)
+        {
+            return false;
+        }
+
+        // check if words is anagram
+        var word1Dictionary = CreateCharDictionary(word1);
+        var word2Dictionary = CreateCharDictionary(word2);
+
+        // check if both dictionaries have the same length
+        if (word1Dictionary.Count != word2Dictionary.Count)
+        {
+            return false;
+        }
+
+        // FOR DEBUGING PURPOSE
+        // Debug.WriteLine("WORD 1 DICTIONARY");
+        // foreach (var pair in word1Dictionary)
+        // {
+        //     Debug.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+        // }
+
+        // Debug.WriteLine("\nWORD 2 DICTIONARY");
+        // foreach (var pair in word2Dictionary)
+        // {
+        //     Debug.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+        // }
+
+        // Check if the two word dictionaries are equal. Return true if they match; otherwise, return false.
+        bool isAnagram = true;
+        foreach (var pair in word1Dictionary)
+        {
+            // first check if dictionary 2 contain the search key from dictionary 1
+            if (!word2Dictionary.ContainsKey(pair.Key))
+            {
+                isAnagram = false;
+            }
+            else
+            {
+                // check if the value in dictionary 2 is same as the value in dictionary 1
+                if (word2Dictionary[pair.Key] != pair.Value)
+                {
+                    isAnagram = false;
+                }
+            }
+        }
+
+        return isAnagram;
+
+        // HELPER FUNCTION
+        static Dictionary<char, int> CreateCharDictionary(string word, bool isCaseSensitive = false, bool ignoreSpace = true)
+        {
+            if (!isCaseSensitive)
+            {
+                // convert word to lowercase
+                word = word.ToLowerInvariant();
+            }
+
+            if (ignoreSpace)
+            {
+                word = word.Replace(" ", "");  // remove white spaces from anywhere in the string
+                // // remove white spaces, tab spaces or any white space at all, which apprears anywhere in the string.
+                // word = Regex.Replace(word, @"\s+", "");
+            }
+
+            // Debug.WriteLine($"Clean Word: {word}");  // for testing purpose
+
+            var wordDictionary = new Dictionary<char, int>();
+
+            foreach (var letter in word)
+            {
+                if (wordDictionary.ContainsKey(letter))
+                {
+                    wordDictionary[letter] += 1;
+                }
+                else
+                {
+                    wordDictionary[letter] = 1;
+                }
+            }
+
+            return wordDictionary;
+        }
     }
 
     /// <summary>
@@ -101,6 +236,15 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+
+        // output each place earthquake has happened today and it's magnitude
+        var earthquakesDescriptions = new List<string>{};
+        foreach (Feature feature in featureCollection.Features)
+        {
+            var formatedString = $"{feature.Properties.Place} - Mag {feature.Properties.Mag}";
+            earthquakesDescriptions.Add(formatedString);
+        }
+
+        return [.. earthquakesDescriptions];
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,5 +1,7 @@
-﻿public static class DisplaySums {
-    public static void Run() {
+﻿public static class DisplaySums
+{
+    public static void Run()
+    {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
         // 6 4
@@ -27,7 +29,26 @@
     /// in the list.
     /// </summary>
     /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
+    private static void DisplaySumPairs(int[] numbers)
+    {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+        // create a set of the numbers
+        // Iterate through the set and check for the sum of 10
+        var UniqueNumbers = new HashSet<int>(numbers);
+        var FoundSumNumbers = new HashSet<int>();
+
+        foreach (int number in UniqueNumbers)
+        {
+            int completeSumNumber = 10 - number;  // get remaining number that is needed to get to 10
+            if (UniqueNumbers.Contains(completeSumNumber) && completeSumNumber != number)
+            {
+                if (!(FoundSumNumbers.Contains(completeSumNumber)  && FoundSumNumbers.Contains(number)))
+                {
+                    FoundSumNumbers.Add(completeSumNumber);
+                    FoundSumNumbers.Add(number);
+                    Console.WriteLine($"{number} + ({completeSumNumber}) = 10");
+                }
+            }
+        }
     }
 }
